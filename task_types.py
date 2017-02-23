92ecec669fe12587aed849f4f9ee11c948365cf1
darkwizz <psyhoge@gmail.com>
2017-02-23 22:37:31 +0200

    fix with list creating and datacenter latency for endpoint

--- task_types.py
+++ task_types.py
@@ -2,13 +2,13 @@ class CacheServer:
     def __init__(self, id, size, endpoints_count):
         self.id = id
         self.size = size
-        self.endpoints = [None] * endpoints_count
+        self.endpoints = [None for _ in range(endpoints_count)]
 
 
 class Endpoint:
     def __init__(self, id, caches_count):
         self.id = id
-        self.caches = [None] * caches_count
+        self.caches = [None for _ in range(caches_count)]
         self.requests = []
         self.videos = []
 
@@ -49,12 +49,12 @@ def get_parsed_items(path):
         for i in range(param_dict['endpoints_count']):
             latency, caches_links_count = map(int, config.readline().split(' '))
             endpoint = Endpoint(id=i, caches_count=param_dict['caches_count'])
-            datacenter.endpoints[i] = latency
+            datacenter.endpoints[i] = endpoint
             for j in range(caches_links_count):
                 cache_id, cache_latency = map(int, config.readline().split(' '))
                 endpoint.caches[cache_id] = cache_latency
                 result['cache_servers'][cache_id].endpoints[i] = endpoint
-            endpoint.caches[-1] = datacenter.size
+            endpoint.caches[-1] = latency
             endpoint.videos = result['videos']
             result['endpoints'][i] = endpoint
         for _ in range(param_dict['request_descriptions_count']):
@@ -64,6 +64,13 @@ def get_parsed_items(path):
                 'requests_count': requests_count
             })
     return result
+
+
+# def write_output_to_file(cache_servers, path):
+#     filtered_caches = [(i, cache) for i, cache in enumerate(cache_servers) if cache[0].fill]
+#     with open(path, 'w') as output_file:
+#         output_file.write(str(len(filtered_caches)) + '\n')
+#         for i in range(len)
 
 
 if __name__ == '__main__':
