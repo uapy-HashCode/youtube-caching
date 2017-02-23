dc35ce95dfcef76d83be69874b96774ef1d82d69
ledidukh <ledidukh@gmail.com>
2017-02-23 22:05:39 +0200

    Added algo script

---
+++ core_algo.py
@@ -0,0 +1,50 @@
+from .task_types import items
+
+videos = items[0]
+end_points = items[1]
+cache_servers = items[2]
+
+
+def process_video(video_weights_per_cache, request, end_point, cache_id):
+
+    video_id = request['video']
+    number_of_request = request['requests_count']
+    l_d = end_point.caches[-1]
+    size = videos[video_id]
+    l_c = end_point.caches[cache_id]
+    saved_time = number_of_request*(l_d-l_c)
+    #if video in bug
+    if video_id in video_weights_per_cache.keys():
+        video_weights_per_cache[video_id][0]+=saved_time
+    else:
+        video_weights_per_cache[video_id] = [saved_time, size, end_point.id]
+    return
+
+configuration = []
+# Run by each cache
+for cache in cache_servers:
+
+    video_weights_per_cache = []
+    # Run by each end_point
+    for end_point in cache.end_points:
+        # Run by each video for end_point
+        for request in end_point.requests:
+            # calculate weight
+            process_video(video_weights_per_cache, request = request, end_point=end_point)
+
+    # sort videos
+    v = sorted(video_weights_per_cache, lambda x: x[0])
+
+    # process cache server
+    cache_config = {'cache_id': cache.id, 'number_of_videos': 0, 'videos': []}
+    packed = 0
+    for i in v:
+        video_weight = i.values()[0][0]
+        if video_weight + packed < cache.weight:
+            cache_config['number_of_videos'] += 1
+            video_id = i.keys()[0]
+            cache_config['videos'].append({'video_id': video_id, 'weight': video_weight})
+            packed += video_weight
+        else:
+            pass
+    configuration.append({'cache_id': cache.id, 'config': cache_config})
